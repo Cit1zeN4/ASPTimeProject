8ed2b522298d18a3dc12cadff1c8100cec8ba303
Cit1zeN4 <set4433@gmail.com>
2019-05-02 00:18:37 +0300

    Added: UserController, View and css Changed: _LoginPartial

---
+++ ASPTime/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace ASPTime.Controllers
+{
+    public class UserController : Controller
+    {
+        [Authorize]
+        public ActionResult UserProfile(string userId)
+        {
+            if (User.Identity.GetUserId() == userId)
+            {
+                return View();
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+    }
+};
