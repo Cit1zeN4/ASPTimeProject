fcd176fac7f492e72c040e36b3e41a647aea19b3
Cit1zeN4 <set4433@gmail.com>
2019-05-01 14:28:54 +0300

    Start

---
+++ ASPTime/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPTime.Startup))]
+namespace ASPTime
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
